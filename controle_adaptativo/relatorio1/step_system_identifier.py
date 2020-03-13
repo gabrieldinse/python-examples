f69fcc8ce1759260d8c8d8900513d44c4d07e361
Gabriel Dinse <gabriel_dinse@hotmail.com>
2020-03-13 18:21:34 -0300

    Fixes issues in _nishikawa function

--- controle_adaptativo/relatorio1/step_system_identifier.py
+++ controle_adaptativo/relatorio1/step_system_identifier.py
@@ -35,13 +35,12 @@ def _sundaresan(output, input_amplitude, sampling_rate):
     for k in range(len(output)):
         if flag1 and flag2:
             break
-        if output[k] >= 0.353 * input_amplitude and not flag1:
+        if output[k] >= 0.353 * output[-1] and not flag1:
             t1 = k / sampling_rate
             flag1 = True
-        if output[k] >= 0.853 * input_amplitude and not flag2:
+        if output[k] >= 0.853 * output[-1] and not flag2:
             t2 = k / sampling_rate
             flag2 = True
-
     gain = (output[-1] - output[0]) / input_amplitude
     time_constant = 0.67 * (t2 - t1)
     delay = 1.3 * t1 - 0.29 * t2
@@ -52,18 +51,17 @@ def _nishikawa(output, input_amplitude, sampling_rate):
     area0 = 0.0
 
     for k, k_prev in zip(range(1, len(output), 1), range(len(output) - 1)):
-        area0 += (output[k] - (output[k] + output[k_prev]) / 2) / sampling_rate
+        area0 += (output[-1] - (output[k] + output[k_prev]) / 2) / sampling_rate
 
     time_array = np.arange(
-        1 / sampling_rate, len(output) / sampling_rate, len(output))
+        1 / sampling_rate, len(output) / sampling_rate, 1 / sampling_rate)
     t0 = area0 / (output[-1] - output[0])
     k0 = np.digitize(t0, time_array)
 
     area1 = 0.0
 
     for k, k_prev in zip(range(1, int(k0), 1), range(int(k0) - 1)):
-        area1 +=  (output[k] - output[k_prev]) / (2 * sampling_rate)
-
+        area1 +=  (output[k] + output[k_prev]) / (2 * sampling_rate)
     gain = (output[-1] - output[0]) / input_amplitude
     time_constant = area1 / (0.368 * (output[-1] - output[0]))
     delay = t0 - time_constant
