1116627afc783173b086312ed3de37b3aa4f5435
Gabriel Dinse <gabriel_dinse@hotmail.com>
2020-03-13 13:39:39 -0300

    Created _nishikawa function in step_system_identifier.py

--- controle_adaptativo/relatorio1/step_system_identifier.py
+++ controle_adaptativo/relatorio1/step_system_identifier.py
@@ -28,23 +28,39 @@ def _sundaresan(output, input_amplitude, sampling_rate):
     for k in range(len(output)):
         if flag1 and flag2:
             break
-
         if output[k] >= 0.353 * input_amplitude and not flag1:
             t1 = k * sampling_rate
             flag1 = True
-
         if output[k] >= 0.853 * input_amplitude and not flag2:
             t2 = k * sampling_rate
             flag2 = True
 
+    gain = (output[-1] - output[0]) / input_amplitude
     time_constant = 0.67 * (t2 - t1)
     delay = 1.3 * t1 - 0.29 * t2
-    gain = (output[-1] - output[0]) / input_amplitude
     return gain, time_constant, delay
 
 
 def _nishikawa(output, input_amplitude, sampling_rate):
-    pass
+    area0 = 0.0
+
+    for k, k_prev in range(1, len(output), 1), range(len(output) - 1):
+        area0 += sampling_rate * (output[k] - (output[k] + output[k_prev]) / 2)
+
+    time_array = np.arange(
+        sampling_rate, len(output) * sampling_rate, len(output))
+    t0 = area0 / (output[-1] - output[0])
+    k0 = np.digiteze(t0, time_array)
+
+    area1 = 0.0
+
+    for k, k_prev in range(1, k0, 1), range(k0 - 1):
+        area1 += sampling_rate * (output[k] - output[k_prev]) / 2
+
+    gain = (output[-1] - output[0]) / input_amplitude
+    time_constant = area1 / (0.368 * (output[-1] - output[0]))
+    delay = t0 - time_constant
+    return gain, time_constant, delay
 
 
 def _smith(output, input_amplitude, sampling_rate):
